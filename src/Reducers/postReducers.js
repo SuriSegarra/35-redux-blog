8d59cbc373fe9fe6ad47bc3214dabb2d72bd0fb2
surelis segarra <sdarianne34@gmail.com>
2020-05-11 15:33:31 -0700

    reducers test passed

--- src/Reducers/postReducers.js
+++ src/Reducers/postReducers.js
@@ -1,12 +1,10 @@
-import { postBlog, deletePost } from '../actions/postActions';
-
 const initialState = [];
 
 export default function reducer(state = initialState, action) {
     switch(action.type) {
-        case postBlog: 
+        case 'POST': 
         return [...state, action.payload];
-        case deletePost:
+        case 'DELETE':
             return state.filter((post, i) => i !== action.payload);
             default:
                 return state;
