c1c44f73ec8ca96abbdc09174bf70925fa0ddb9a
surelis segarra <sdarianne34@gmail.com>
2020-05-11 16:06:46 -0700

    fixed variables name

--- src/Selectors/postSelectors.js
+++ src/Selectors/postSelectors.js
@@ -1,6 +1,6 @@
 export const getPosts = state => state;
 
 export const getPostByIndex = (state, i) => {
-    const getPostByIndex = getPosts(state).findIndex(post => post === state[i]);
-    return state[getPostByIndex];
+    const foundByIndex = getPosts(state).findIndex(post => post === state[i]);
+    return state[foundByIndex];
 };
