82c35bcc355488b52df5d0e99a824294437e7a92
surelis segarra <sdarianne34@gmail.com>
2020-05-11 16:02:57 -0700

    gets a post by index test passed

--- src/Selectors/postSelectors.test.js
+++ src/Selectors/postSelectors.test.js
@@ -19,4 +19,32 @@ describe('posts selectors', () => {
             }
         ]);
     });
+
+    it('gets a post by index', () => {
+        const state = [
+    {
+        title: 'test post title 1',
+        body: 'test body 1'
+      },
+      {
+        title: 'test post title 2',
+        body: 'test body 2'
+      },
+      {
+        title: 'test post title 3',
+        body: 'test body 3'
+      },
+      {
+        title: 'test post title 4',
+        body: 'test body 4'
+        }
+      ];
+
+      const post = getPostByIndex(state, 2);
+
+      expect(post).toEqual({
+        title: 'test post title 3',
+        body: 'test body 3'
+      });
+    });
 });
